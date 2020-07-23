2354e205c4c4b73c6fee85491c047d67a3381254
Thivi <theviyanthan@outlook.com>
2020-07-23 19:05:55 +0530

    Add summary step to remote userstore wizard.

--- apps/admin-portal/src/components/user-stores/wizards/summary-remote-userstore.tsx
+++ apps/admin-portal/src/components/user-stores/wizards/summary-remote-userstore.tsx
@@ -16,8 +16,90 @@
  * under the License.
  */
 
-import React, { FunctionComponent, ReactElement } from "react";
+import { TestableComponentInterface } from "@wso2is/core/models";
+import { CopyInputField, EmptyPlaceholder } from "@wso2is/react-components";
+import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
+import { Grid, List, Segment } from "semantic-ui-react";
+import { listAgents } from "../../../api";
+import { EmptyPlaceholderIllustrations } from "../../../configs";
+import { AccessTokenPostBody, ConnectedAgent } from "../../../models";
 
-export const SummaryRemoteUserstore: FunctionComponent = (): ReactElement => {
-    return <div>x</div>;
+interface SummaryRemoteUserstorePropsInterface extends TestableComponentInterface {
+    userstoreData: AccessTokenPostBody;
+}
+
+export const SummaryRemoteUserstore: FunctionComponent<SummaryRemoteUserstorePropsInterface> = (
+    props: SummaryRemoteUserstorePropsInterface
+): ReactElement => {
+    const { userstoreData, [ "data-testid" ]: testId } = props;
+    const [ connectedAgents, setConnectedAgents ] = useState<ConnectedAgent[]>([]);
+
+    useEffect(() => {
+        listAgents(userstoreData.domain).then((response: ConnectedAgent[]) => {
+            setConnectedAgents(response);
+        });
+    }, [ userstoreData.domain ]);
+
+    return (
+        <Grid className="wizard-summary" data-testid={ testId }>
+            <Grid.Row>
+                <Grid.Column mobile={ 16 } tablet={ 16 } computer={ 16 } textAlign="center">
+                    <div className="general-details">
+                        <h3 data-testid={ `${testId}-userstore-domain` }>{ userstoreData?.domain }</h3>
+                    </div>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row className="summary-field" columns={ 2 }>
+                <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 7 } textAlign="right">
+                    <div className="label">Userstore Secret</div>
+                </Grid.Column>
+                <Grid.Column className="overflow-wrap" mobile={ 16 } tablet={ 8 } computer={ 8 } textAlign="left">
+                    <div className="value">
+                        <CopyInputField
+                            data-testid={ `${testId}-userstore-secret` }
+                            value={ userstoreData?.token }
+                            showSecretText="Show secret"
+                            hideSecretText="Hide secret"
+                            secret={ true }
+                        />
+                    </div>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={ 2 }>
+                <Grid.Column mobile={ 16 } tablet={ 8 } computer={ 7 } textAlign="right">
+                    <div className="label">Connected Agents</div>
+                </Grid.Column>
+                <Grid.Column className="overflow-wrap" mobile={ 16 } tablet={ 8 } computer={ 8 } textAlign="left">
+                    { connectedAgents.length > 0 ? (
+                        <List data-testid={ `${testId}-agent-list` }>
+                            { connectedAgents.map((agent: ConnectedAgent, index: number) => {
+                                return (
+                                    <List.Item key={ index }>
+                                        <List.Icon name="circle" color={ agent.status === "C" ? "green" : "red" } />
+                                        <List.Content>
+                                            <List.Header>{ agent.node }</List.Header>
+                                        </List.Content>
+                                    </List.Item>
+                                );
+                            }) }
+                        </List>
+                    ) : (
+                            <Segment placeholder>
+                                <EmptyPlaceholder
+                                    subtitle={ null }
+                                    title="No agent is connected"
+                                    image={ EmptyPlaceholderIllustrations.newList }
+                                    imageSize="tiny"
+                                    data-testid={ `${testId}-empty-agent-list` }
+                                />
+                            </Segment>
+                        ) }
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+};
+
+SummaryRemoteUserstore.defaultProps = {
+    "data-testid": "remote-userstore-summary"
 };
