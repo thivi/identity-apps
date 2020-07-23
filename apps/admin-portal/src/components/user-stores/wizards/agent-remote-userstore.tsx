b0293b848c55a2a4f0eb0ce6e287dc6cf82c4fd3
Thivi <theviyanthan@outlook.com>
2020-07-23 19:05:35 +0530

    Add agent step to remote userstore wizard

--- apps/admin-portal/src/components/user-stores/wizards/agent-remote-userstore.tsx
+++ apps/admin-portal/src/components/user-stores/wizards/agent-remote-userstore.tsx
@@ -16,8 +16,129 @@
  * under the License.
  */
 
-import React, { FunctionComponent, ReactElement } from "react";
+import { TestableComponentInterface } from "@wso2is/core/models";
+import { CopyInputField, EmptyPlaceholder, Hint } from "@wso2is/react-components";
+import React, { FunctionComponent, ReactElement, useCallback, useEffect, useState } from "react";
+import { Button, Divider, Grid, Icon, List, Message, Segment } from "semantic-ui-react";
+import { listAgents, retrieveFilePath } from "../../../api";
+import { EmptyPlaceholderIllustrations } from "../../../configs";
+import { AccessTokenPostBody, ConnectedAgent } from "../../../models";
 
-export const AgentRemoteUserstore: FunctionComponent = (): ReactElement => {
-    return <div></div>;
+interface AgentRemoteUserstorePropsInterface extends TestableComponentInterface {
+    userstoreData: AccessTokenPostBody;
+}
+
+export const AgentRemoteUserstore: FunctionComponent<AgentRemoteUserstorePropsInterface> = (
+    props: AgentRemoteUserstorePropsInterface
+): ReactElement => {
+    const [ filePath, setFilePath ] = useState("");
+    const { userstoreData, [ "data-testid" ]: testId } = props;
+    const [ connectedAgents, setConnectedAgents ] = useState<ConnectedAgent[]>([]);
+    const [ errorMessage, setErrorMessage ] = useState("");
+    const [ errorDescription, setErrorDescription ] = useState("");
+
+    /**
+     * Fetches the list of connected agents.
+     */
+    const getListOfConnectedAgents = useCallback((): void => {
+        listAgents(userstoreData.domain)
+            .then((response: ConnectedAgent[]) => {
+                setConnectedAgents(response);
+                setErrorDescription("");
+                setErrorMessage("");
+            })
+            .catch((error) => {
+                setErrorMessage(error?.response?.message);
+                setErrorDescription(error?.response?.description ?? error?.message);
+            });
+    }, [ userstoreData ]);
+
+    useEffect(() => {
+        retrieveFilePath().then((response) => {
+            setFilePath(response?.location);
+        });
+
+        getListOfConnectedAgents();
+    }, [ getListOfConnectedAgents ]);
+
+    return (
+        <Grid>
+            <Grid.Row columns={ 1 }>
+                <Grid.Column>
+                    <Message info>
+                        Download the onprem userstore agent and run it. Once the agent successfully establishes a
+                        connection with our servers, you will be able to find it in the list below.
+                    </Message>
+                </Grid.Column>
+            </Grid.Row>
+            <Divider hidden />
+            <Grid.Row columns={ 1 }>
+                <Grid.Column textAlign="center">
+                    <Button size="large" as="a" href={ filePath } download="wso2agent.zip">
+                        <Icon name="download" />
+                        <span>Download wso2agent.zip</span>
+                    </Button>
+                </Grid.Column>
+            </Grid.Row>
+            <Divider hidden />
+            <Grid.Row>
+                <Grid.Column width={ 8 }>
+                    <CopyInputField
+                        data-testid={ `${testId}-userstore-secret` }
+                        value={ userstoreData?.token }
+                        showSecretText="Show secret"
+                        hideSecretText="Hide secret"
+                        secret={ true }
+                    />
+                    <Hint>Copy the userstore secret and provide it to the agent during startup.</Hint>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={ 1 }>
+                <Grid.Column>
+                    <h4>Connected Agents</h4>
+                    <Segment>
+                        { connectedAgents.length > 0 ? (
+                            <List data-testid={ `${testId}-agent-list` }>
+                                { connectedAgents.map((agent: ConnectedAgent, index: number) => {
+                                    return (
+                                        <List.Item key={ index }>
+                                            <List.Icon name="circle" color={ agent.status === "C" ? "green" : "red" } />
+                                            <List.Content>
+                                                <List.Header>{ agent.node }</List.Header>
+                                            </List.Content>
+                                        </List.Item>
+                                    );
+                                }) }
+                            </List>
+                        ) : (
+                                <EmptyPlaceholder
+                                    subtitle={ [ "Download the agent and run it to connect to our servers." ] }
+                                    title="No agent is connected"
+                                    image={ EmptyPlaceholderIllustrations.newList }
+                                    imageSize="tiny"
+                                    data-testid={ `${testId}-empty-agent-list` }
+                                />
+                            ) }
+                    </Segment>
+                    { (errorMessage || errorDescription) && (
+                        <Message negative>
+                            { errorMessage && <Message.Header>{ errorMessage }</Message.Header> }
+                            <Message.Content>{ errorDescription }</Message.Content>
+                        </Message>
+                    ) }
+                    <Button onClick={ getListOfConnectedAgents } data-testid={ `${testId}-refresh-list` }>
+                        <Icon name="refresh" />
+                        Refresh list
+                    </Button>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+};
+
+/**
+ * The default props of the component
+ */
+AgentRemoteUserstore.defaultProps = {
+    "data-testid": "remote-userstore-agent"
 };
