2ad9e4207b66a56a6b15b5fece1d05beadf9a54d
Thivi <theviyanthan@outlook.com>
2020-07-23 22:10:06 +0530

    Add remote userstore edit tabs

--- apps/admin-portal/src/pages/user-stores/user-stores-edit.tsx
+++ apps/admin-portal/src/pages/user-stores/user-stores-edit.tsx
@@ -1,39 +1,41 @@
 /**
-* Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* WSO2 Inc. licenses this file to you under the Apache License,
-* Version 2.0 (the 'License'); you may not use this file except
-* in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the 'License'); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import { AlertLevels, TestableComponentInterface } from "@wso2is/core/models";
 import { addAlert } from "@wso2is/core/store";
 import { PageLayout, ResourceTab } from "@wso2is/react-components";
-import React, { FunctionComponent, ReactElement, useEffect, useState } from "react"
+import React, { FunctionComponent, ReactElement, useEffect, useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { RouteComponentProps } from "react-router";
 import { Image } from "semantic-ui-react";
-import { getAType, getAUserStore } from "../../api";
+import { getAType, getAUserStore, retrieveRemoteUserstore, getUserStores } from "../../api";
 import {
     EditBasicDetailsUserStore,
     EditConnectionDetails,
     EditGroupDetails,
-    EditUserDetails
+    EditUserDetails,
+    EditRemoteUserstoreGeneral,
+    EditRemoteUserstoreAgents
 } from "../../components";
 import { DatabaseAvatarGraphic } from "../../configs";
 import { history } from "../../helpers";
-import { CategorizedProperties, UserStore, UserstoreType } from "../../models";
+import { AccessTokenPostBody, CategorizedProperties, UserStore, UserstoreType } from "../../models";
 import { reOrganizeProperties } from "../../utils";
 
 /**
@@ -58,17 +60,16 @@ interface RouteParams {
 const UserStoresEditPage: FunctionComponent<UserStoresEditPageInterface> = (
     props: UserStoresEditPageInterface & RouteComponentProps<RouteParams>
 ): ReactElement => {
-
-    const {
-        match,
-        [ "data-testid" ]: testId
-    } = props;
+    const { match, [ "data-testid" ]: testId } = props;
 
     const userStoreId = match.params.id;
 
     const [ userStore, setUserStore ] = useState<UserStore>(null);
     const [ type, setType ] = useState<UserstoreType>(null);
     const [ properties, setProperties ] = useState<CategorizedProperties>(null);
+    const [ isRemote, setIsRemote ] = useState(false);
+    const [ remoteUserstore, setRemoteUserstore ] = useState<AccessTokenPostBody>(null);
+    const [ remoteUserstoreId, setRemoteUserstoreId ] = useState<string>("");
 
     const dispatch = useDispatch();
 
@@ -77,42 +78,82 @@ const UserStoresEditPage: FunctionComponent<UserStoresEditPageInterface> = (
     /**
      * Fetches the userstore by its id
      */
-    const getUserStore = () => {
-        getAUserStore(userStoreId).then(response => {
-            setUserStore(response);
-        }).catch(error => {
-            dispatch(addAlert(
-                {
-                    description: error?.description
-                        || t("adminPortal:components.userstores.notifications.fetchUserstores.genericError" +
-                            ".description"),
-                    level: AlertLevels.ERROR,
-                    message: error?.message
-                        || t("adminPortal:components.userstores.notifications.fetchUserstores.genericError.message")
-                }
-            ));
-        })
-    };
+    const getUserStore = useCallback(
+        (id?: string) => {
+            if (id) {
+                retrieveRemoteUserstore(id).then((response: AccessTokenPostBody) => {
+                    setRemoteUserstore(response);
+                });
+            } else {
+                getAUserStore(userStoreId)
+                    .then((response) => {
+                        setUserStore(response);
+                    })
+                    .catch((error) => {
+                        dispatch(
+                            addAlert({
+                                description:
+                                    error?.description ||
+                                    t(
+                                        "adminPortal:components.userstores.notifications.fetchUserstores.genericError" +
+                                        ".description"
+                                    ),
+                                level: AlertLevels.ERROR,
+                                message:
+                                    error?.message ||
+                                    t(
+                                        "adminPortal:components.userstores.notifications.fetchUserstores." +
+                                        "genericError.message"
+                                    )
+                            })
+                        );
+                    });
+            }
+        },
+        [ dispatch, t, userStoreId ]
+    );
 
     useEffect(() => {
-        getUserStore();
-    }, []);
+        if (match.params.id) {
+            const userstoreId = match.params.id.split("-");
+            if (userstoreId.shift() === "remote") {
+                const id = userstoreId.join("-");
+                setRemoteUserstoreId(id);
+                setIsRemote(true);
+                //get remote userstore
+                getUserStore(id);
+            } else {
+                setIsRemote(false);
+                getUserStore();
+            }
+        }
+    }, [ match.params.id, getUserStore ]);
 
     useEffect(() => {
         if (userStore) {
-            getAType(userStore?.typeId, null).then((response) => {
-                setType(response);
-            }).catch(error => {
-                dispatch(addAlert({
-                    description: error?.description
-                        || t("adminPortal:components.userstores.notifications.fetchUserstoreMetadata." +
-                            "genericError.description"),
-                    level: AlertLevels.ERROR,
-                    message: error?.message
-                        || t("adminPortal:components.userstores.notifications.fetchUserstoreMetadata" +
-                            ".genericError.message")
-                }));
-            });
+            getAType(userStore?.typeId, null)
+                .then((response) => {
+                    setType(response);
+                })
+                .catch((error) => {
+                    dispatch(
+                        addAlert({
+                            description:
+                                error?.description ||
+                                t(
+                                    "adminPortal:components.userstores.notifications.fetchUserstoreMetadata." +
+                                    "genericError.description"
+                                ),
+                            level: AlertLevels.ERROR,
+                            message:
+                                error?.message ||
+                                t(
+                                    "adminPortal:components.userstores.notifications.fetchUserstoreMetadata" +
+                                    ".genericError.message"
+                                )
+                        })
+                    );
+                });
         }
     }, [ userStore ]);
 
@@ -125,85 +166,101 @@ const UserStoresEditPage: FunctionComponent<UserStoresEditPageInterface> = (
     /**
      * The tab panes
      */
-    const panes = [
-        {
-            menuItem:  t ("adminPortal:components.userstores.pageLayout.edit.tabs.general"),
-            render: () => (
-                <EditBasicDetailsUserStore
-                    properties={ properties?.basic }
-                    userStore={ userStore }
-                    update={ getUserStore }
-                    id={ userStoreId }
-                    data-testid={ `${ testId }-userstore-basic-details-edit` }
-                />
-            )
-        },
-        {
-            menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.connection"),
-            render: () => (
-                <EditConnectionDetails
-                    update={ getUserStore }
-                    type={ type }
-                    id={ userStoreId }
-                    properties={ properties?.connection }
-                    data-testid={ `${ testId }-userstore-connection-details-edit` }
-                />
-            )
-        },
-        {
-            menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.user"),
-            render: () => (
-                <EditUserDetails
-                    update={ getUserStore }
-                    type={ type }
-                    id={ userStoreId }
-                    properties={ properties?.user }
-                    data-testid={ `${ testId }-userstore-user-details-edit` }
-                />
-            )
-        },
-        {
-            menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.group"),
-            render: () => (
-                <EditGroupDetails
-                    update={ getUserStore }
-                    type={ type }
-                    id={ userStoreId }
-                    properties={ properties?.group }
-                    data-testid={ `${ testId }-userstore-group-details-edit` }
-                />
-            )
-        }
-    ];
+    const panes = isRemote
+        ? [
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.general"),
+                render: () => (
+                    <EditRemoteUserstoreGeneral
+                        userstoreData={ remoteUserstore }
+                        onUpdate={ () => {
+                            getUserStore(remoteUserstoreId);
+                        } }
+                    />
+                )
+            },
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.general"),
+                render: () => (
+                    <EditRemoteUserstoreAgents
+                        userstoreData={ remoteUserstore }
+                    />
+                )
+            }
+        ]
+        : [
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.general"),
+                render: () => (
+                    <EditBasicDetailsUserStore
+                        properties={ properties?.basic }
+                        userStore={ userStore }
+                        update={ getUserStore }
+                        id={ userStoreId }
+                        data-testid={ `${testId}-userstore-basic-details-edit` }
+                    />
+                )
+            },
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.connection"),
+                render: () => (
+                    <EditConnectionDetails
+                        update={ getUserStore }
+                        type={ type }
+                        id={ userStoreId }
+                        properties={ properties?.connection }
+                        data-testid={ `${testId}-userstore-connection-details-edit` }
+                    />
+                )
+            },
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.user"),
+                render: () => (
+                    <EditUserDetails
+                        update={ getUserStore }
+                        type={ type }
+                        id={ userStoreId }
+                        properties={ properties?.user }
+                        data-testid={ `${testId}-userstore-user-details-edit` }
+                    />
+                )
+            },
+            {
+                menuItem: t("adminPortal:components.userstores.pageLayout.edit.tabs.group"),
+                render: () => (
+                    <EditGroupDetails
+                        update={ getUserStore }
+                        type={ type }
+                        id={ userStoreId }
+                        properties={ properties?.group }
+                        data-testid={ `${testId}-userstore-group-details-edit` }
+                    />
+                )
+            }
+        ];
 
     return (
         <PageLayout
             image={
-                <Image
-                    floated="left"
-                    verticalAlign="middle"
-                    rounded
-                    centered
-                    size="tiny"
-                >
+                <Image floated="left" verticalAlign="middle" rounded centered size="tiny">
                     <DatabaseAvatarGraphic />
                 </Image>
             }
-            title={ userStore?.name }
+            title={ isRemote ? remoteUserstore?.domain : userStore?.name }
             description={ t("adminPortal:components.userstores.pageLayout.edit.description") }
             backButton={ {
                 onClick: () => {
                     history.push("/user-stores");
                 },
-                text: t ("adminPortal:components.userstores.pageLayout.edit.back")
+                text: t("adminPortal:components.userstores.pageLayout.edit.back")
             } }
             titleTextAlign="left"
             bottomMargin={ false }
-            data-testid={ `${ testId }-page-layout` }
+            data-testid={ `${testId}-page-layout` }
         >
-            <ResourceTab panes={ panes } data-testid={ `${ testId }-tabs` }/>
+            <ResourceTab panes={ panes } data-testid={ `${testId}-tabs` } />
         </PageLayout>
-    )
+    );
 };
 
 /**
